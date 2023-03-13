2fe4debddee41fefe1d467d4b673ace0094d766b
João Sales <jfsjfsales@mail.com>
2023-03-13 13:08:01 -0300

    Adicianando respostal dos Exercicios Parte1

--- ExecíciosSprin3-Python/ExercíciosParte1-Python/Exercício1-Python.py
+++ ExecíciosSprin3-Python/ExercíciosParte1-Python/Exercício1-Python.py
@@ -1,2 +1,11 @@
-Desenvolva um código Python que lê do teclado nome e a idade atual de uma pessoa. Como saída, imprima o ano em que a pessoa completará 100 anos de idade.
+# Desenvolva um código Python que lê do teclado nome e a idade atual de uma pessoa. 
+# Como saída, imprima o ano em que a pessoa completará 100 anos de idade.
 
+
+
+nome = input("Digite seu nome: ")
+idade_atual = int(input("Digite sua idade: "))
+ano_atual = 2023
+ano_completara_100 = ano_atual + (100 - idade_atual)
+
+print(ano_completara_100)
