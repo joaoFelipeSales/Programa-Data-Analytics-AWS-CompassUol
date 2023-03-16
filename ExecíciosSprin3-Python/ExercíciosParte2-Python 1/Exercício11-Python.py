09c6b9fc953c0ac717b6279f613a84d0a140eeef
João Sales <jfsjfsales@mail.com>
2023-03-16 08:13:29 -0300

    atualizando formatacao dos enunciandos

--- ExecíciosSprin3-Python/ExercíciosParte2-Python 1/Exercício11-Python.py
+++ ExecíciosSprin3-Python/ExercíciosParte2-Python 1/Exercício11-Python.py
@@ -1,9 +1,16 @@
-# Escreva um programa que lê o conteúdo do arquivo texto arquivo_texto.txt e imprime o seu conteúdo.
+""" Escreva um programa que lê o conteúdo do arquivo texto arquivo_texto.txt e imprime o seu conteúdo."""
 
 
-with open("arquivo_texto.txt", "r") as f:
-    conteudo = f.read()
-    print(conteudo)
+arquivo = open('arquivo_texto.txt', 'r')
+linha = arquivo.readline()
+while linha != '':
+    print(linha, end='')
+    linha = arquivo.readline()
+arquivo.close()
+
+
+
+
 
 
 
--- ExecíciosSprin3-Python/ExercíciosParte2-Python 1/Exercício12-Python.py
+++ ExecíciosSprin3-Python/ExercíciosParte2-Python 1/Exercício12-Python.py
@@ -1,10 +1,8 @@
-# Leia o arquivo person.json, faça o parsing e imprima seu conteúdo.
+"""Leia o arquivo person.json, faça o parsing e imprima seu conteúdo."""
 
 import json
 
-# Abre o arquivo person.json e faz o parsing do conteúdo em um dicionário Python
 with open('person.json', 'r') as f:
     person = json.load(f)
 
-# Imprime o conteúdo do dicionário
 print(person)
