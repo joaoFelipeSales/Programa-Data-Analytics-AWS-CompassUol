e05e52fc5b5b3c1729cebfa3108c6c78484edb5b
João Sales <jfsjfsales@mail.com>
2023-03-27 13:12:35 -0300

    adicionando exercícios da Sprint4

---
+++ ExercíciosSprint4-PythonFuncional/Exercício1-PythonFuncional.py
@@ -0,0 +1,5 @@
+""" Você está recebendo um arquivo contendo 10.000 números inteiros, um em cada linha. 
+    Utilizando lambdas e high order functions, apresente os 5 maiores valores pares e a soma destes.
+    Você deverá aplicar as seguintes funções no exercício: map-filter-sorted-sum
+    Seu código deverá exibir na saída (simplesmente utilizando 2 comandos `print()`):
+    a lista dos 5 maiores números pares em ordem decrescente e a soma destes valores. """
