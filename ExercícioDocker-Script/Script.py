54cdd11dae5cf2cc71fa895b728d84fefbbd3dd4
João Sales <jfsjfsales@mail.com>
2023-04-04 10:00:02 -0300

    atualizando codigo

--- ExercícioDocker-Script/Script.py
+++ ExercícioDocker-Script/Script.py
@@ -4,4 +4,4 @@ while True:
     string = input("Digite uma palavra para gerar o hash: ")
     hash_obj = hashlib.sha1(string.encode())
     hash_str = hash_obj.hexdigest()
-    print("O hash :", hash_str)
+    print("O hash é:", hash_str)
