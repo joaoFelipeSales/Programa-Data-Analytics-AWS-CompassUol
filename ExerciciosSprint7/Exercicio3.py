507a76ce3c49c04c68867128674de33edba1e428
João Sales <jfsjfsales@mail.com>
2023-05-03 08:25:01 -0300

    atualizando exercicio3

--- ExerciciosSprint7/Exercicio3.py
+++ ExerciciosSprint7/Exercicio3.py
@@ -1,2 +1,15 @@
 # Apresente o nome do ator/atriz com a maior média por filme.
 
+import pandas as pd
+
+# Carrega o arquivo CSV com os dados dos atores e atrizes
+df = pd.read_csv('actors.csv')
+
+# Agrupa os dados pelo nome do ator/atriz e calcula a média por filme
+media_por_ator = df.groupby('Actor')['Average per Movie'].mean()
+
+# Obtém o nome do ator/atriz com a maior média por filme
+ator_com_media_mais_alta = media_por_ator.idxmax()
+
+# Imprime o resultado
+print(ator_com_media_mais_alta)
