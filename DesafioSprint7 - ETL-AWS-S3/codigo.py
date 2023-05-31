eeb93b00a9666f4b6abc8e409d51bcc194e7cb31
João Sales <jfsjfsales@mail.com>
2023-05-31 12:28:56 -0300

    adicionando certificado

--- DesafioSprint7 - ETL-AWS-S3/codigo.py
+++ DesafioSprint7 - ETL-AWS-S3/codigo.py
@@ -2,7 +2,13 @@ import boto3
 
 s3 = boto3.resource("s3")
 
-bucket = s3.Bucket("compassuol")
+bucket_name = "compassuol"
 
-bucket.upload_file(Key="movies.csv", Filename="movies.csv")
-bucket.upload_file(Key="series.csv", Filename="series.csv")
+movies_key = "Raw/Local/CSV/Movies/2022/05/02/movies.csv"
+series_key = "Raw/Local/CSV/Series/2022/05/02/series.csv"
+
+bucket = s3.Bucket(bucket_name)
+
+bucket.upload_file(Filename="movies.csv", Key=movies_key)
+bucket.upload_file(Filename="series.csv", Key=series_key)
+
